81747b5ea15aa5809c0119b6beaca88c2d889b93
jayaramsivaramannair <jayaramsivaramannair@gmail.com>
2021-01-23 05:49:24 -0500

    Installed react-router-dom package and axios package

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
+import {BrowserRouter as Router} from 'react-router-dom';
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <Router>
+      <div>
+        <h1>Lambda Eats</h1>
+      </div>
+    </Router>
   );
 };
 export default App;
